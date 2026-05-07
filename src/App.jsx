88224c2716cb8cd88e4c9c8e5bd107941a700b85
Ahmad34-90 <ahmiiikha@gmail.com>
2026-05-07 11:23:08 +0500

    last learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const name = "Ahmad";
-  const age= 12;
+function TodoList(){
+  const todos=[
+    {id: 1, text:"learn REact."},
+    {id: 2, text: "HEllo Ahmad."},
+    {id: 3, text: "Ding work."},
+  ];
 
   return (
-    <>
-      <div className="heading"><h1>Hello React!</h1> <p>hello {name}! Your name is {age}?</p></div>
-    </>
+    <div className="div">
+      <h2>My React Todo's</h2>
+      <ul>
+        {todos.map((todo)=>(
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
+// function Greeting({name}){
+//   return <p>Hello {name} you are greatd in React!</p>
+// }
+// function App() {
+//   const [name, setName]= useState("");
 
-export default App
+//   function handleChange(event){
+//     const value =event.target.value;
+//     setName(value)
+//   }
+  
+
+//   return (
+//     <div>
+//         <input type="text" placeholder='Enter Name...' onChange={handleChange}/>
+//         <p>{name}</p>
+//     </div>
+//   )
+// }
+
+export default TodoList;
