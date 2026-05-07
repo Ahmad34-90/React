ec050af07109a4d525eabfeed92f7bba4a5c6593
Ahmad34-90 <ahmiiikha@gmail.com>
2026-05-07 07:33:20 +0500

    Inital react setup!

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const name = "Ahmad";
+  const age= 12;
+
+  return (
+    <>
+      <div className="heading"><h1>Hello React!</h1> <p>hello {name}! Your name is {age}?</p></div>
+    </>
+  )
+}
+
+export default App
